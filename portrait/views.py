e5512422119776cfc1aae5068fe95cb29967c29a
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-16 20:36:55 +0300

    error in portrait view

--- portrait/views.py
+++ portrait/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, APIView
+from rest_framework.decorators import api_view, APIView, authentication_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Portrait as Portrait_Model, Comment
 from .serializers import PortraitSerializer, CommentsSerializer
 # Create your views here.
 
 class Portratis(APIView):
+    # permission_classes = [IsAuthenticated]
+    # authentication_classes = [SessionAuthentication, TokenAuthentication]
+
     def get(self, request):
         portrait_objs = Portrait_Model.objects.all()
         serializer = PortraitSerializer(portrait_objs, many=True)
@@ -16,18 +20,15 @@ class Portratis(APIView):
 
     def post(self, request):
         mutible_data = request.data.copy()
-        token = mutible_data.get("token")
-        if token:
-            token = Token.objects.filter(key=token).first()
-            if token:
-                mutible_data["owner"] = token.user.id
-                serializer = PortraitSerializer(data=mutible_data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
-                else:
-                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error":"you must authorize"}, status=status.HTTP_403_FORBIDDEN)
+        mutible_data["owner"] = request.user
+        serializer = PortraitSerializer(data=mutible_data)
+        if serializer.is_valid():
+            print(serializer.data)
+            print(serializer.is_valid())
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CommnetView(APIView):
 
