1a066e9c89561e3ce2867ecf7f8a1e622b5e555e
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-16 18:56:32 +0300

    register with image done

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,14 +6,9 @@ from django.contrib.auth import authenticate, login as django_login
 from .models import UserProfile
 
 class RegisterSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(max_length=250)
-    last_name = serializers.CharField(max_length=100, required=False)
-    first_name = serializers.CharField(max_length=100, required=False)
-
     class Meta:
         model = UserProfile
-        fields = ["username", "password", "last_name", "first_name", "image"]
+        fields = ["username", "password", "last_name", "first_name", "email", "image"]
 
     def validate_username(self, username):
         user = User.objects.filter(username=username).first()
@@ -21,21 +16,21 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("this username is already taken")
         return username
 
-    def create(self, validated_data):
-        print("validated_data :", validated_data)
-        new_user = UserProfile.objects.create(username=validated_data.get("username"))
-        new_user.set_password(validated_data.get("password"))
-        if validated_data.get("last_name"):
-            new_user.last_name = validated_data.get("last_name")
-        if validated_data.get("first_name"):
-            new_user.first_name = validated_data.get("first_name")
-
-        photo_file = validated_data.get("image")
-
-        if photo_file:
-            new_user.image = photo_file
-
-        new_user.save()
+    # def create(self, validated_data):
+    #     print("validated_data :", validated_data)
+    #     new_user = UserProfile.objects.create(username=validated_data.get("username"))
+    #     new_user.set_password(validated_data.get("password"))
+    #     if validated_data.get("last_name"):
+    #         new_user.last_name = validated_data.get("last_name")
+    #     if validated_data.get("first_name"):
+    #         new_user.first_name = validated_data.get("first_name")
+    #
+    #     photo_file = validated_data.get("image")
+    #
+    #     if photo_file:
+    #         new_user.image = photo_file
+    #
+    #     new_user.save()
 
 
 
@@ -51,6 +46,8 @@ class LoginSerializer(serializers.Serializer):
 
     def create_token(self, data, request):
         user = authenticate(username=data.get("username"), password=data.get("password"))
+        print(data.get("username"))
+        print(data.get("password"))
         django_login(request, user=user)
         token = Token.objects.filter(user=user).first()
         if not token:
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-import base64
 from django.contrib.auth import authenticate, login as django_login
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,9 +11,8 @@ class RegisterView(APIView):
 	def post(self, request):
 		data = request.data
 		serializer = RegisterSerializer(data=data)
-		print(data)
 		if serializer.is_valid():
-			serializer.create(serializer.data)
+			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
